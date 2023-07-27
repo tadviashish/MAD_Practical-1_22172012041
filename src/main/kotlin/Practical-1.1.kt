579b1c26312e2bb5f0f888b966377b8f28611d2a
Ashish <139306500+tadviashish@users.noreply.github.com>
2023-07-28 00:45:56 +0530

    Update Practical-1.1.kt

--- src/main/kotlin/Practical-1.1.kt
+++ src/main/kotlin/Practical-1.1.kt
@@ -1,12 +1,14 @@
 fun main()
 {
-    var a = 10;
-    var b = 0.2
-    var c = 54546.1
-    var d = 525685323
-    var e = 'A'
-    var f = "Ashish"
-    var g = false
+    var a:Int = 10;
+    var b:Float = 1.2F
+    var c:Double = 5454.52
+    var d:Long = 525685323
+    var e:Char = 'A'
+    var f:String = "Ashish"
+    var g:Boolean = false
+    var h:Short = -23
+    var i:Byte = 64
 
     println("$a is integer")
     println("$b is float")
@@ -15,5 +17,7 @@ fun main()
     println("$e is character")
     println("$f is string")
     println("$g is boolean")
+    println("$h is Short")
+    println("$i is Byte")
 
 }
